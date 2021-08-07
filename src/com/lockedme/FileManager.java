b21d3cd15d09b7f02d8bc41d9b11ce39f124e440
hnarayanan11 <hnarayanan11@gmail.com>
2021-08-07 20:34:31 +0530

    Writing content into file

--- src/com/lockedme/FileManager.java
+++ src/com/lockedme/FileManager.java
@@ -1,29 +1,63 @@
 package com.lockedme;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileManager {
 	/**
 	 * This method will return the file names from the folder
+	 * 
 	 * @param folderPath
 	 * @return List<String>
 	 */
 	public static List<String> getAllFilenames(String folderPath) {
-		//Creating File Object
+		// Creating File Object
 		File fl = new File(folderPath);
-		
-		//Getting all the files into FileArray
+
+		// Getting all the files into FileArray
 		File[] listOfFiles = fl.listFiles();
-		
-		//Declare a list to store file names
+
+		// Declare a list to store file names
 		List<String> fileNames = new ArrayList<>();
-		
-		for(File f: listOfFiles)
+
+		for (File f : listOfFiles)
 			fileNames.add(f.getName());
-			
-		//return File Name list
+
+		// return File Name list
 		return fileNames;
+	}
+
+	/**
+	 * This method will create or append content into the file specified
+	 * 
+	 * @param folderPath
+	 * @param fileName
+	 * @param content
+	 * @return boolean
+	 */
+	public static boolean createToFile(String folderPath, String fileName, List<String> content) {
+
+		try {
+			// Creating File Object
+			File fl = new File(folderPath, fileName);
+			// Creating file wirter object to
+			FileWriter fow = new FileWriter(fl);
+
+			//Writing content into file
+			for (String s : content)
+				fow.write(s + "\n");
+
+			fow.close();
+			return true;
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+
 	}
 }
--- src/com/lockedme/LockedMeProj.java
+++ src/com/lockedme/LockedMeProj.java
@@ -1,6 +1,8 @@
 package com.lockedme;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class LockedMeProj {
 
@@ -12,6 +14,35 @@ public class LockedMeProj {
 		List<String> fileNames = FileManager.getAllFilenames(folderPath);
 		// Printing File Names
 		fileNames.forEach(System.out::println);
+		
+		//Variable Declaration
+		Scanner scn = new Scanner(System.in);
+		String fileName =""; 
+		int linesCount=0;
+		List<String> content = new ArrayList<>();
+		
+		//Read file name from User
+		System.out.println("Enter the File Name:");
+		fileName=scn.nextLine();
+		
+		//Read number of lines from user
+		System.out.println("Enter how many lines in the file:");
+		linesCount=Integer.parseInt(scn.nextLine());
+		
+		//Read Lines form User
+		for(int i=1;i<=linesCount;i++) {
+			System.out.println("Enter line "+i+":");
+			content.add(scn.nextLine());
+		}
+		
+		//save the content into the file
+		boolean isSaved = FileManager.createToFile(folderPath, fileName, content);
+		
+		if(isSaved)
+			System.out.println("File and data saved successfully");
+		else 
+			System.out.println("Somer error occured. Please admint@lockme.com");
+		scn.close();		
 	}
 
 }
